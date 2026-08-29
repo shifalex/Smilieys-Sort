74e51ef4bc33a551ee48c4f166c71901470afeda
shifalex <90042959+shifalex@users.noreply.github.com>
2026-08-29 19:05:46 +0300

    Fix iPad installed app launch

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "smileys-sorting-game-v231";
+const CACHE_NAME = "smileys-sorting-game-v232";
 const APP_SHELL = [
-  "./",
-  "./index.html",
-  "./styles.css?v=ipad-feedback-v231-20260829",
-  "./app.js?v=ipad-feedback-v231-20260829",
-  "./manifest.webmanifest",
-  "./icons/icon-180.png",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "/",
+  "/index.html",
+  "/styles.css?v=ipad-feedback-v231-20260829",
+  "/app.js?v=ipad-feedback-v231-20260829",
+  "/manifest.webmanifest",
+  "/icons/icon-180.png",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
@@ -43,7 +43,7 @@ self.addEventListener("fetch", (event) => {
           caches.open(CACHE_NAME).then((cache) => cache.put("./index.html", copy));
           return response;
         })
-        .catch(() => caches.match(event.request).then((cachedResponse) => cachedResponse || caches.match("./index.html")))
+        .catch(() => caches.match("/index.html"))
     );
     return;
   }
